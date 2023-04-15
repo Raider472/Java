00ee6f1698d0875cf8710c3deffd62c3403a579a
Raider472 <gabrielnae1@gmail.com>
2023-04-15 11:16:25 +0200

    TD2

--- TD2/src/TD2.java
+++ TD2/src/TD2.java
@@ -1,8 +1,2 @@
-import java.util.Arrays;
-import java.util.Scanner;
-public class TD2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] tableau = {7,8,10,9,15};
-    }
+package PACKAGE_NAME;public class TD2 {
 }
