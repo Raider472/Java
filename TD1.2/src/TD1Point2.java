c444817a54ecb5d8183b49a7245eb482068a194a
Raider472 <gabrielnae1@gmail.com>
2023-04-15 10:06:21 +0200

    TD2

--- TD1.2/src/TD1Point2.java
+++ TD1.2/src/TD1Point2.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class TD1Point2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] tabTab = {10,20,30,40,50};
-        System.out.println(ex1(tabTab, scanner));
+        String[] tabS = {"d", "f", "i"};
+        int[] tableau = new int[5];
+        System.out.println("choisis l'indice");
+        int indice = scanner.nextInt();
+        if (indice >= 0 && indice < tableau.length) {
+            System.out.println("choisis un nombre");
+            int nbChoisis = scanner.nextInt();
+            System.out.println("Avant la modification "+Arrays.toString(tableau));
+            tableau[indice] = nbChoisis;
+            System.out.println("Apres la modif "+Arrays.toString(tableau));
+        }
+        else {
+            System.out.println("l'indice n'est pas dans la taille");
+        }
+        //System.out.println(ex1(tabTab, scanner));
+        //System.out.println(ex2(tabTab, scanner));
     }
 
     public static boolean ex1(int[] tab, Scanner scanner) {
@@ -15,5 +31,16 @@ public class TD1Point2 {
             }
         }
         return false;
+    }
+
+    public static int ex2(int[] tab, Scanner scanner) {
+        System.out.println("Choisis le nombre num");
+        int nbChoisis = scanner.nextInt();
+        for(int i = 0; i < tab.length; i++) {
+            if(tab[i] == nbChoisis) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
